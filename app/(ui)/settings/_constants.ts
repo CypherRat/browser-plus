f82d696c356081dd3087922d5e59257ca90ce611
Abhimanyu Kumar <88980877+CypherRat@users.noreply.github.com>
2023-12-04 18:16:03 +0530

    Added local constants for settings

---
+++ app/(ui)/settings/_constants.ts
@@ -0,0 +1,14 @@
+export const clockVals = [
+  { label: "Digital", disabled: false },
+  { label: "Analog", disabled: false },
+];
+
+export const yesNoVals = [
+  { label: "No", disabled: false },
+  { label: "Yes", disabled: false },
+];
+
+export const dateTimeFormatVals = [
+  { label: "12hr", disabled: false },
+  { label: "24hr", disabled: false },
+];
