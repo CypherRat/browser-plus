4d07e93f4648a3842e4000a4b45cb4eeae5d0201
Abhimanyu Kumar <88980877+CypherRat@users.noreply.github.com>
2023-12-05 16:26:24 +0530

    Created supporting utils and constants for settings page

--- app/(ui)/settings/_constants.ts
+++ app/(ui)/settings/_constants.ts
@@ -12,3 +12,10 @@ export const dateTimeFormatVals = [
   { label: "12hr", disabled: false },
   { label: "24hr", disabled: false },
 ];
+
+export const searchEngineVals = [
+  { label: "Google", disabled: false },
+  { label: "Bing", disabled: false },
+  { label: "DuckDuckGo", disabled: false },
+  { label: "Yahoo", disabled: false },
+];
