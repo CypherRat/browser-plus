0bab644bf1af9b7e83a725bb8e0296aa5edeee37
Abhimanyu Kumar <88980877+CypherRat@users.noreply.github.com>
2023-12-05 16:27:00 +0530

    Updated useLocalStorage.ts hook

--- app/_hook/useLocalStorage.ts
+++ app/_hook/useLocalStorage.ts
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 
-export default function useLocalStorage(key: string, initialValue: any = "") {
+export default function useLocalStorage(key: string, initialValue: any = {}) {
   const [storedValue, setStoredValue] = useState(initialValue);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       const item = window.localStorage.getItem(key);
-      setStoredValue(item ? JSON.parse(item) : initialValue);
+      if (item) {
+        setStoredValue(JSON.parse(item));
+      } else {
+        window.localStorage.setItem(key, JSON.stringify(initialValue));
+        setStoredValue(initialValue);
+      }
     }
   }, [key, initialValue]);
 
