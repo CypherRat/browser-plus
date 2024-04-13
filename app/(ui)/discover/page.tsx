b0b7626711743e5acd756bf2faaf2cfbc13353b0
Abhimanyu Kumar <88980877+CypherRat@users.noreply.github.com>
2024-04-14 01:05:52 +0530

    Create Discover Route

---
+++ app/(ui)/discover/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Discover() {
+  return (
+    <>
+      <div>Discover Page</div>
+      <Link href="/settings">Go to Settings</Link>
+    </>
+  );
+}
